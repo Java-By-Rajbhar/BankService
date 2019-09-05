7fc15623011955346bdbe9f18f3cf2632fae9060
User1 <User1@LP-5CD9176M8Z.HCLT.CORP.HCL.IN>
2019-09-05 12:43:54 +0530

    Added port num

--- src/main/java/com/ing/bank/controller/IbanController.java
+++ src/main/java/com/ing/bank/controller/IbanController.java
@@ -2,6 +2,9 @@ package com.ing.bank.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class IbanController {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(IbanController.class);
-	
+
+	@GetMapping("/bankApi")
+	public ResponseEntity<String> callBankApi(){
+		
+		LOGGER.info("IbanController :: callBankApi --");
+		return new ResponseEntity<String>("Hello! Bank Api is Up", HttpStatus.OK);
+	}
 }
--- src/main/java/com/ing/bank/entity/IbanCode.java
+++ src/main/java/com/ing/bank/entity/IbanCode.java
@@ -1,6 +1,9 @@
 package com.ing.bank.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -16,6 +19,8 @@ import lombok.Setter;
 @Table
 public class IbanCode {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer ibanId;
 	private Integer ibanCode;
 	private String bankName;
